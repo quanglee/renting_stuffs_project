09c7d1b6b8c2e6d6de4be7a0d324bb4a1b7981b2
Nguyen <nguyenv19@student.douglascollege.ca>
2019-07-11 10:19:19 -0700

    added User model

---
+++ frontend/app/src/main/java/com/quangle/rentingutilities/core/model/User.java
@@ -0,0 +1,69 @@
+package com.quangle.rentingutilities.core.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    //user fields
+    private int id;
+    private String email, username;
+    private double lat, lng;
+    private boolean isActive, isAdmin;
+
+    //constructor
+    public User() {}
+
+    //getter & setter
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+}
