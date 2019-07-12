c6c390a893a9436737aed19928627253dfe6f60b
Fernando <ferchriquelme@gmail.com>
2019-07-12 16:43:24 -0700

    Splash screen

--- frontend/app/src/main/java/com/quangle/rentingutilities/ui/SplashActivity.java
+++ frontend/app/src/main/java/com/quangle/rentingutilities/ui/SplashActivity.java
@@ -1,13 +1,12 @@
-package com.quangle.rentingutilities;
+package com.quangle.rentingutilities.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
