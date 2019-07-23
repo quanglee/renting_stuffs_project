1cbabe90d889201725470013c9123f6146649d27
Fershark <ferchriquelme@gmail.com>
2019-07-23 15:42:45 -0700

    Register added
    - Create a new user and then login user

--- frontend/app/src/main/java/com/quangle/rentingutilities/networking/Api.java
+++ frontend/app/src/main/java/com/quangle/rentingutilities/networking/Api.java
@@ -2,8 +2,7 @@ package com.quangle.rentingutilities.networking;
 
 import com.quangle.rentingutilities.core.model.Auth;
 import com.quangle.rentingutilities.core.model.Item;
-
-import org.json.JSONArray;
+import com.quangle.rentingutilities.core.model.User;
 
 import java.util.HashMap;
 import java.util.List;
@@ -18,6 +17,11 @@ public interface Api {
     @POST("auth/login")
     Call<Auth> login(@Body HashMap<String, String> params);
 
+    //Users
+    @POST("users/create")
+    Call<User> createUser(@Body HashMap<String, String> params);
+
+    //Items
     // get all items for home page
     @GET("items")
     Call<List<Item>> getAllItems();
