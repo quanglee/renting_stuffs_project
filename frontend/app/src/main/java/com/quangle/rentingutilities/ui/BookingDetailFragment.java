77510124a36bb9b98897b82677266539a2e1f73b
VuDuyHauNguyen <nguyenv19@student.douglascollege.ca>
2019-08-01 09:55:40 -0700

    added accept/reject/done buttons in booking/request detail

--- frontend/app/src/main/java/com/quangle/rentingutilities/ui/BookingDetailFragment.java
+++ frontend/app/src/main/java/com/quangle/rentingutilities/ui/BookingDetailFragment.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 import com.quangle.rentingutilities.R;
 import com.quangle.rentingutilities.core.model.Auth;
 import com.quangle.rentingutilities.core.model.Booking;
+import com.quangle.rentingutilities.viewmodel.BookingViewModel;
 import com.quangle.rentingutilities.viewmodel.ItemViewModel;
 import com.squareup.picasso.Picasso;
 
@@ -27,8 +28,9 @@ public class BookingDetailFragment extends BaseFragment {
     private static final String ARG_BOOKING = "booking";
     private TextView txtNameView, txtDescView, txtPriceView, txtStatusView;
     private ImageView imageView;
-    private Button btnCancel, btnReview;
+    private Button btnCancel, btnReview, btnAccept, btnReject, btnDone;
     private ItemViewModel itemViewModel;
+    private BookingViewModel bookingViewModel;
 
 
 
@@ -56,6 +58,9 @@ public class BookingDetailFragment extends BaseFragment {
         imageView = view.findViewById(R.id.itemImage);
         btnCancel = view.findViewById(R.id.btnCancelBooking);
         btnReview = view.findViewById(R.id.btnReviewBooking);
+        btnAccept = view.findViewById(R.id.btnAcceptBooking);
+        btnReject = view.findViewById(R.id.btnRejectBooking);
+        btnDone = view.findViewById(R.id.btnDoneBooking);
 
         // load booking data
         txtNameView.setText(booking.getItem().getName());
@@ -67,17 +72,34 @@ public class BookingDetailFragment extends BaseFragment {
 
         btnCancel.setVisibility(View.INVISIBLE);
         btnReview.setVisibility(View.INVISIBLE);
+        btnAccept.setVisibility(View.INVISIBLE);
+        btnReject.setVisibility(View.INVISIBLE);
+        btnDone.setVisibility(View.INVISIBLE);
+
         // check to display approriate button
         if (booking.getStatus().equals(Booking.STATUS.PENDING.displayName())) {
+            //TODO: display if user is borrower
             btnCancel.setVisibility(View.VISIBLE);
+
+            //TODO: display if user is owner of the item
+            btnAccept.setVisibility(View.VISIBLE);
+            btnReject.setVisibility(View.VISIBLE);
+
         } else if (booking.getStatus().equals(Booking.STATUS.DONE.displayName())) {
+
+            //TODO: display if user is borrower and have no review yet
             btnReview.setVisibility(View.VISIBLE);
-        } else {
+        } else if(booking.getStatus().equals(Booking.STATUS.ACCEPTED.displayName())) {
+
+            //TODO: display if user is owner of the item
+            btnDone.setVisibility(View.VISIBLE);
+        }else{
             // do not display any buttons
         }
 
         // cancel booking
         itemViewModel = ViewModelProviders.of(getActivity()).get(ItemViewModel.class);
+        bookingViewModel = ViewModelProviders.of(getActivity()).get(BookingViewModel.class);
 
         btnCancel.setOnClickListener(v -> {
             btnCancel.setEnabled(false);
@@ -86,6 +108,24 @@ public class BookingDetailFragment extends BaseFragment {
             itemViewModel.cancelBooking(String.valueOf(booking.getId())).observe(this, authNetworkResource -> {
                 hideProgressBar();
                 btnCancel.setEnabled(true);
+
+                if (authNetworkResource.code == 401) {
+                    Toast.makeText(getContext(), "Cancel Booking Fail!", Toast.LENGTH_SHORT).show();
+                } else if (authNetworkResource.data != null) {
+
+                    //return to booking tab
+                    BaseActivity.startHomeActivityAtTab(HomeActivity.BOOKINGS_TAB_INDEX);
+                }
+            });
+        });
+
+        btnAccept.setOnClickListener(v -> {
+            btnAccept.setEnabled(false);
+            showProgressBar();
+
+            bookingViewModel.cancelBooking(String.valueOf(booking.getId())).observe(this, authNetworkResource -> {
+                hideProgressBar();
+                btnAccept.setEnabled(true);
 
                 if (authNetworkResource.code == 401) {
                     Toast.makeText(getContext(), "Cancel Booking Fail!", Toast.LENGTH_SHORT).show();
