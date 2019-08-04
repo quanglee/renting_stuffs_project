969c48282fe624ca79e2f7844321506f77072e31
VuDuyHauNguyen <nguyenv19@student.douglascollege.ca>
2019-08-04 13:08:43 -0700

    modified mock-up data; user1 has 5 items (1-5); user2 has 5 items (6-10); user2 booking 3 items (1-3) of user1

--- backend/src/database/bookings.sql
+++ backend/src/database/bookings.sql
@@ -7,14 +7,7 @@ create table bookings (
     returnDate      DATETIME,
     constraint      FK_BOOKING_ITEM foreign key (itemId) references items (id)
 );
--- user6 to user10 booking 50 items from user1 to user5
-insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (1, 1, 'user6@shareandget.com', 'Pending', '2019-08-01 00:00:00', '2019-08-03 00:00:00');
-insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (2, 2, 'user7@shareandget.com', 'Pending', '2019-08-01 00:00:00', '2019-08-03 00:00:00');
-insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (3, 3, 'user7@shareandget.com', 'Pending', '2019-08-01 00:00:00', '2019-08-03 00:00:00');
-insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (4, 4, 'user8@shareandget.com', 'Pending', '2019-08-01 00:00:00', '2019-08-03 00:00:00');
-insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (5, 5, 'user7@shareandget.com', 'Pending', '2019-08-01 00:00:00', '2019-08-03 00:00:00');
-insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (6, 6, 'user6@shareandget.com', 'Pending', '2019-08-01 00:00:00', '2019-08-03 00:00:00');
-insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (7, 7, 'user8@shareandget.com', 'Pending', '2019-08-01 00:00:00', '2019-08-03 00:00:00');
-insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (8, 8, 'user9@shareandget.com', 'Pending', '2019-08-01 00:00:00', '2019-08-03 00:00:00');
-insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (9, 9, 'user9@shareandget.com', 'Pending', '2019-08-01 00:00:00', '2019-08-03 00:00:00');
-insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (10, 10, 'user7@shareandget.com', 'Pending', '2019-08-01 00:00:00', '2019-08-03 00:00:00');
+-- user2 booking 3 items from user1
+insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (1, 1, 'user2@shareandget.com', 'Pending', '2019-08-10 08:00:00', '2019-08-13 20:00:00');
+insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (2, 2, 'user2@shareandget.com', 'Pending', '2019-08-10 08:00:00', '2019-08-13 20:00:00');
+insert into bookings (id, itemId, borrowerId, status, startDate, returnDate) values (3, 3, 'user2@shareandget.com', 'Pending', '2019-08-10 08:00:00', '2019-08-13 20:00:00');
